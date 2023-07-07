a45d11b5fe352a3a0e4b446c68b3ff6da91cdcf5
Gabriel Nascimento <135087316+gabrielgnsilva@users.noreply.github.com>
2023-07-07 01:05:21 -0300

    Fixed floating window not following when changing to prev group

--- data/config/qtile/config.py
+++ data/config/qtile/config.py
@@ -206,7 +206,7 @@ def moveFloatingWindowOnGroupChangePrev(qtile):
     for window in qtile.current_group.windows:
 
         if window.floating is not True or \
-           window.qtile.current_layout.name != 'floating':
+           window.qtile.current_layout.name == 'floating':
             continue
 
         for g in qtilegroups:

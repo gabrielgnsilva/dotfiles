776c37882a06b8c5ef776f9ab567ad9f0f81e6bb
Gabriel Nascimento <135087316+gabrielgnsilva@users.noreply.github.com>
2026-08-23 15:47:41 -0300

    feat: add keybinding to floaterm

--- .config/nvim/lua/plugins/lazy/floaterm-nvim.lua
+++ .config/nvim/lua/plugins/lazy/floaterm-nvim.lua
@@ -1,6 +1,26 @@
 return {
   'gabrielgnsilva/floaterm.nvim',
   cmd = 'FloatingTerminal',
-  keys = '<leader>t',
+  keys = { '<leader>t', '<leader>T' },
   opts = {},
+  config = function(_, opts)
+    local floaterm = require('floaterm')
+
+    floaterm.setup(opts)
+
+    require('utils.mappings').load_keymap({
+      {
+        mode = { 'n' },
+        bindings = {
+          {
+            key = '<leader>T',
+            cmd = function()
+              floaterm.toggle({ use_oil_cwd = true })
+            end,
+            desc = 'Open terminal in oil.nvim directory',
+          },
+        },
+      },
+    })
+  end,
 }
